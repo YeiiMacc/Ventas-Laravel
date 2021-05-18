361c384ec251cd1cd083bc05baf751eee1042e09
YeiiMacc <yeiimacc22@outlook.es>
2021-05-17 21:01:29 -0500

    Relaciones y Tablas Pivotes. Muchos a Muchos de Products a Cart(CartProductTable). Muchos a Muchos de Products a Order(OrderProductTable).

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Cart;
+use App\Models\Order;
 
 class Product extends Model
 {
@@ -21,4 +23,14 @@ class Product extends Model
         'stock',
         'status',
     ];
+
+    public function carts()
+    {
+        return $this->belongsToMany(Cart::class)->withPivot('quantity');
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class)->withPivot('quantity');
+    }
 }
