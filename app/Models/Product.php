0f6f63d2c15e6f1c3bc16278cf5bc61f0daf40f3
YeiiMacc <yeiimacc22@outlook.es>
2021-06-28 14:07:19 -0500

    Soft Deleting para remover productos.
    Ignorar el global scope de SoftDeletes
    Product::withoutGlobalScopes()->onlyTrashed()->get();
    
    restaurar el primero
    Product::withoutGlobalScopes()->onlyTrashed()->first()->restore();
    
    restaurar todos
    Product::withoutGlobalScopes()->onlyTrashed()->restore();

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Cart;
 use App\Models\Order;
 use App\Models\Image;
@@ -11,7 +12,7 @@ use App\Scopes\AvailableScope;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'products';
 
     // Solucion a N+1 consultas DB
